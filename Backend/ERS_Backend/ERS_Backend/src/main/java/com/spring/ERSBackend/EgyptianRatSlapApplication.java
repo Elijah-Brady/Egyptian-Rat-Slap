f40b6b8f06eaaebb5f29794aadc6c86ea4311a6a
Elijah-Brady <117706158+Elijah-Brady@users.noreply.github.com>
2023-05-12 17:09:16 -0500

    Fixing file structure

--- Backend/ERS_Backend/ERS_Backend/src/main/java/com/spring/ERSBackend/EgyptianRatSlapApplication.java
+++ Backend/ERS_Backend/ERS_Backend/src/main/java/com/spring/ERSBackend/EgyptianRatSlapApplication.java
@@ -2,16 +2,15 @@ package com.spring.ERSBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author dinok
  */
 @SpringBootApplication
-public class CardPracticeApplication {
+public class EgyptianRatSlapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CardPracticeApplication.class, args);
+		SpringApplication.run(EgyptianRatSlapApplication.class, args);
 	}
 
 }
